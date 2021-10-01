12cdd5ce661829f4e4e38f9eaad63b002ab29547
L Prathyusha <83442712+Prathyusha-L@users.noreply.github.com>
2021-10-01 09:40:33 +0530

    Add files via upload

---
+++ Constants.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	// int constant
+	const int intVal = 10;
+
+	// Real constant
+	const float floatVal = 4.14;
+
+	// char constant
+	const char charVal = 'A';
+
+	// string constant
+	const char stringVal[10] = "ABC";
+	
+	printf("Integer constant:%d \n", intVal );
+	printf("Floating point constant: %.2f\n", floatVal );
+	printf("Character constant: %c\n", charVal );
+	printf("String constant: %s\n", stringVal);
+	
+	return 0;
+}
+
+
+
+
+
